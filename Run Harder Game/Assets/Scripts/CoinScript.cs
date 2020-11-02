b036ebf31c72637ac17d61ff42b487a718f18a77
Osman <osman103@msn.com>
2020-11-02 10:51:12 +0000

    Main

---
+++ Run Harder Game/Assets/Scripts/CoinScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinScript : MonoBehaviour
+{
+
+    private LevelManager gameLevelManager;
+    public int coinValue;
+
+    public int score = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameLevelManager = FindObjectOfType<LevelManager>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other) //when the player collides with the coin
+    {
+        gameLevelManager.AddCoins(coinValue); //updates the scoore method
+        Destroy(gameObject);
+        score++;
+        Debug.Log("Score is: " + score);
+
+    }
+}
---
+++ Run Harder Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelManager : MonoBehaviour
+{
+	public int coins;
+	public Text coinText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    	coinText.text = "Score: " + coins; //displays empty score at start of game
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddCoins(int numberOfCoins)
+    {
+    	coins+=numberOfCoins;
+    	coinText.text = "Score: " + coins; //displays updated score
+
+    }
+}
